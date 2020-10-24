72ba7e68678d296ed52817aaff03e4a9c0e6897e
ritikvashistha <ritik@sutratechlabs.com>
2020-10-25 02:25:17 +0530

    Updates

---
+++ precache-manifest.10a1d32cc58e11e0de0885a7451a00f5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba028bd8ceb04792123201f7f57299a",
+    "url": "/ritikportfolio/index.html"
+  },
+  {
+    "revision": "071ae405ac41341fe456",
+    "url": "/ritikportfolio/static/css/main.b93b1529.chunk.css"
+  },
+  {
+    "revision": "7888ca2df98e02fc3098",
+    "url": "/ritikportfolio/static/js/2.ba62c0ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ritikportfolio/static/js/2.ba62c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071ae405ac41341fe456",
+    "url": "/ritikportfolio/static/js/main.3c68de23.chunk.js"
+  },
+  {
+    "revision": "2881c60c6eb19b4e6808",
+    "url": "/ritikportfolio/static/js/runtime-main.377dc971.js"
+  },
+  {
+    "revision": "3702665fe7977b191d1512be6e71739c",
+    "url": "/ritikportfolio/static/media/default1.3702665f.png"
+  },
+  {
+    "revision": "3d02e1a8d793336a22c549aa05b85993",
+    "url": "/ritikportfolio/static/media/default2.3d02e1a8.png"
+  },
+  {
+    "revision": "07dfbe8d827cc67fa9eab8396a4048e2",
+    "url": "/ritikportfolio/static/media/default3.07dfbe8d.png"
+  },
+  {
+    "revision": "7da94822ef51520f0a6254f986c6fda8",
+    "url": "/ritikportfolio/static/media/defaultB.7da94822.jpg"
+  }
+]);
